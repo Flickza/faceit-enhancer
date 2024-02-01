6ac7afce3f65e62102a54a0e0b2efc829f39fe1a
Adel Johan Holm Gundersen <adelholmg@gmail.com>
2024-02-01 08:59:20 +0100

    wip

--- src/content/features/add-esea-matches-statistics.js
+++ src/content/features/add-esea-matches-statistics.js
@@ -4,6 +4,7 @@ import {
   hasFeatureAttribute,
   setFeatureAttribute,
 } from '../helpers/dom-element'
+import { getMatch, getSelf } from '../helpers/faceit-api'
 
 const FEATURE_ATTRIBUTE = 'ESEA-STATS'
 
@@ -34,6 +35,35 @@ export default async () => {
   const recentMatchesTable = tableContainer
 
   const rows = recentMatchesTable?.children[1].children
+  const theadRows = recentMatchesTable?.children[0].children[0]
+  if (theadRows.children.length === 4) {
+    theadRows.insertBefore(
+      <th className={`${theadRows.children[0].className}`}>Score</th>,
+      theadRows.children[2],
+    )
+  }
+
+  // biome-ignore lint/style/useConst: <explanation>
+  for (let row of rows) {
+    const roomUrl = row.children[3].children[0].children[0]?.href
+    const regex = /en\/cs2\/room\/([^\/]+)(\/|$)/
+    const regexMatch = roomUrl.match(regex)
+    if (regexMatch === null) {
+      break
+    }
+    const match = await getMatch(regexMatch[1])
+    if (!match) {
+      break
+    }
+    const scoreElement = (
+      <td className={row.children[0].className}>
+        {match.clientCustom.team1Score} / {match.clientCustom.team2Score}
+      </td>
+    )
+    if (row.children.length === 4) {
+      row.insertBefore(scoreElement, row.children[3])
+    }
+  }
 
   const ESEA_MATCHES_TABLE = playerNameElement?.parentElement?.parentElement
 
