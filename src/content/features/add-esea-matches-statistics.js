4f4fd4946896d9d5ff5ee87b2b61b20a8629babc
Adel Johan Holm Gundersen <adelholmg@gmail.com>
2024-02-02 11:32:30 +0100

    Refactor ESEA matches statistics feature

--- src/content/features/add-esea-matches-statistics.js
+++ src/content/features/add-esea-matches-statistics.js
@@ -33,51 +33,73 @@ export default async () => {
   }
 
   const recentMatchesTable = tableContainer
+  if (hasFeatureAttribute(FEATURE_ATTRIBUTE, recentMatchesTable)) {
+    return
+  }
 
   const rows = recentMatchesTable?.children[1].children
   const theadRows = recentMatchesTable?.children[0].children[0]
   if (theadRows.children.length === 4) {
     theadRows.insertBefore(
-      <th className={`${theadRows.children[0].className}`}>Score</th>,
-      theadRows.children[2],
+      <>
+        <th className={`${theadRows.children[0].className}`}>Score</th>
+        <th className={`${theadRows.children[0].className}`}>Map</th>
+      </>,
+      theadRows.children[3],
     )
   }
-
-  // biome-ignore lint/style/useConst: <explanation>
+  // biome-ignore lint/style/useConst: <Not using const in a for loop>
   for (let row of rows) {
+    if (hasFeatureAttribute(FEATURE_ATTRIBUTE, row)) {
+      continue
+    }
     const roomUrl = row.children[3].children[0].children[0]?.href
     const regex = /en\/cs2\/room\/([^\/]+)(\/|$)/
     const regexMatch = roomUrl.match(regex)
     if (regexMatch === null) {
       break
     }
-    const match = await getMatch(regexMatch[1])
-    if (!match) {
-      break
-    }
-    const scoreElement = (
-      <td className={row.children[0].className}>
-        {match.clientCustom.team1Score} / {match.clientCustom.team2Score}
-      </td>
-    )
-    if (row.children.length === 4) {
-      row.insertBefore(scoreElement, row.children[3])
+    try {
+      getMatch(regexMatch[1]).then((match) => {
+        if (!match) {
+          return
+        }
+        let scoreElement
+        if (!match.clientCustom && match.state === 'FINISHED') {
+          scoreElement = (
+            <>
+              <td className={row.children[0].className}>Walkover</td>
+              <td className={row.children[0].className} />
+            </>
+          )
+        } else if (match.state === 'SCHEDULED') {
+          scoreElement = (
+            <>
+              <td className={row.children[0].className} />
+              <td className={row.children[0].className} />
+            </>
+          )
+        } else {
+          scoreElement = (
+            <>
+              <td className={row.children[0].className}>
+                {match.clientCustom.team1Score} /{' '}
+                {match.clientCustom.team2Score}
+              </td>
+              <td className={row.children[0].className}>
+                {match.clientCustom.map}
+              </td>
+            </>
+          )
+        }
+        if (row.children.length === 4) {
+          row.insertBefore(scoreElement, row.children[3])
+        }
+        setFeatureAttribute(FEATURE_ATTRIBUTE, row)
+      })
+    } catch (e) {
+      console.error(e)
     }
   }
-
-  const ESEA_MATCHES_TABLE = playerNameElement?.parentElement?.parentElement
-
   setFeatureAttribute(FEATURE_ATTRIBUTE, recentMatchesTable)
-
-  const statisticsElement = (
-    <div
-      style={{
-        marginBottom: 4,
-      }}
-    >
-      {createFeaturedPlayerBadgeElement(playerBadge)}
-    </div>
-  )
-
-  ESEA_MATCHES_TABLE.prepend(statisticsElement)
 }
